fca02cff42e2d52344d92d1686cb144dce98a554
Yotam <doron.yotam@googlemail.com>
2013-04-21 21:53:32 +0200

    Update flickr.py

--- flickr.py
+++ flickr.py
@@ -7,9 +7,9 @@ import os
 
 API_KEY = "KEY GOES HERE"
 
-def retrieve(n_images, tags,directory):
+def retrieve(n_images, tags, directory):
     flickr = flickrapi.FlickrAPI(API_KEY)
-    for p in itertools.islice(flickr.walk(tag_mode='all',tags=','.join(tags),extras="url_l"),n_images):
+    for p in itertools.islice(flickr.walk(tag_mode='all' ,tags=','.join(tags), extras="url_l"), n_images):
         url =  'http://farm%s.static.flickr.com/%s/%s_%s_%s.jpg' % \
         (p.get('farm'), p.get('server'), p.get('id'), p.get('secret'), "b")
         print url
@@ -17,17 +17,17 @@ def retrieve(n_images, tags,directory):
 
 def main():
     parser = argparse.ArgumentParser(description='Download images from Flickr.')
-    parser.add_argument('-t,', '--tags',dest='tags', nargs='+',required=True,
+    parser.add_argument('-t,', '--tags', dest='tags', nargs='+', required=True,
     help='tags used to select images')
-    parser.add_argument('-n','--quantity',type=int,dest='quantity',default=5,
+    parser.add_argument('-n','--quantity', type=int, dest='quantity', default=5,
     help='number of images required (optional, defaults to 5)')
-    parser.add_argument('-o','--output-dir',dest='directory',default=os.path.curdir,
+    parser.add_argument('-o','--output-dir', dest='directory', default=os.path.curdir,
     help='output directory (optional, defaults to current)')
     args = parser.parse_args()
     
     if os.path.exists(os.path.expanduser(args.directory)):
         print 'retrieving %d photos tagged with %s' % (args.quantity, ', '.join(args.tags))
-        retrieve(args.quantity,args.tags,args.directory)
+        retrieve(args.quantity, args.tags, args.directory)
     else:
         print 'invalid directory'
 
